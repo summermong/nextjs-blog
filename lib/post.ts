1dd63ccecebebcdbb6638dc8ec4d7df822aaadde
summermong <2514o_o@naver.com>
2023-07-12 01:40:17 +0900

    포스트 데이터를 가져와서 보여주기

--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remarkHtml from "remark-html";
+import { remark } from "remark";
 
 // 현재 경로에 'posts' 경로 추가
 const postsDirectory = path.join(process.cwd(), "posts");
@@ -20,16 +22,50 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...(matterResult.data as { data: string; title: string }),
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
 
   // Sorting
   return allPostsData.sort((a, b) => {
-    if (a.data < b.data) {
+    if (a.date < b.date) {
       return 1;
     } else {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  // 파일 이름이 fileNames 배열로 들어옴
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileNames) => {
+    return {
+      params: {
+        id: fileNames.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  // remark 사용 전 모듈 다운 (remark-html)
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,47 @@
+import { getSortedPostsData, getAllPostIds, getPostData } from "@/lib/post";
+import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+import Head from "next/head";
 
-const Post = () => {
-  return <div>[id]</div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering'}, {id: 'ssg-ssr}}]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
